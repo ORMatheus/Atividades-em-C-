7bfe2953a93557008bbea8455f60512e5b823a11
MatheusOliveira <matheus.rdeoliveira77@gmail.com>
2025-12-18 17:26:52 -0300

    construtor padrão

--- ClassMusic/music.cpp
+++ ClassMusic/music.cpp
@@ -1,1 +1,9 @@
 #include "music.h"
+
+Music::Music(string NomeMusic,string Artista,string Album,string Ano,float Nota){
+    this -> nomeMusic=NomeMusic;
+    this-> artista=Artista;
+    this ->album=Album;
+    this->ano=Ano;
+    this->nota=Nota;
+}
