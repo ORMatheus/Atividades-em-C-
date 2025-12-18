db79649fb22553fd7dedd4cb8a006f9da5e61a9f
MatheusOliveira <matheus.rdeoliveira77@gmail.com>
2025-12-18 17:18:30 -0300

    Definição para todos os getters

--- ClassMusic/music.h
+++ ClassMusic/music.h
@@ -9,4 +9,16 @@ class Music{
         string album;
         string ano;
         float nota;
+    
+    public:
+    Music(string nomeMusic,string artista,string album,string ano, float nota);
+    
+    string getNomeMusic()const;
+    string getNomeArtista() const;
+    string getNomeAlgum() const ;
+    string getAno() const ;
+    float getNota() const;
+
+    //setters
+    
 };
