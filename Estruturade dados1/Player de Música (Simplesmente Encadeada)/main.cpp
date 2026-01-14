69f44b43fadfa1495d4cb878fbbea28f28dbd348
ORMatheus <Matheus.rdeolivera77@gmail.com>
2026-01-14 09:52:22 -0300

    Implementação da logica de loop para o usuario digitar os dados da musica de maneira interaitova

--- Estruturade dados1/Player de Música (Simplesmente Encadeada)/main.cpp
+++ Estruturade dados1/Player de Música (Simplesmente Encadeada)/main.cpp
@@ -1,6 +1,7 @@
 #include "music.h"
 #include "playlist.h"
 #include <cctype>
+#include <limits>
 
 using namespace std;
 
@@ -14,19 +15,45 @@ string funcNormalize(const string& texto){
 
 int main(){
     Playlist playlist;
+    cout << "Deseja adicionar uma música ?(S/N)"<< endl;
+    char continuar;
+    cin >> continuar;
+    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    while(continuar == 's' || continuar == 'S'){
+        string nome;
+        string artista;
+        cout << "Digite o nome da Musica:";
+        getline(cin,nome);
+        cout << "\nDigite o nome do Artista:";
+        getline(cin,artista);
+       cout << "Deseja colocar as demais informacoes? (s/n): ";
+        char opInfo;
+        cin >> opInfo;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    Music m1("Numb", "Linkin Park");
-    Music m2("NUMB", "Linkin Park");
-
-    playlist.adicionarMusica(m1);
-
-    if (playlist.existeMusica("numb", funcNormalize)) {
-        cout << "Musica ja existe!\n";
-    } else {
-        playlist.adicionarMusica(m2);
+        if(opInfo == 's'|| opInfo == 'S' ){
+            string album;
+            string ano;
+            float nota;
+            cout << "\nDigite o Nome do album: ";
+            getline(cin,album);
+            cout << "Digite o ano de lançamento da Música: ";
+            getline(cin,ano);
+            cout << "\nDigite a nota de 0.0 ate 10.0: "; 
+            cin >> nota;
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            Music m(nome,artista,album,ano,nota);
+            playlist.adicionarMusica(m);
+        }else {
+            Music m(nome,artista);
+            playlist.adicionarMusica(m);
+        }
+        cout << "\nDeseja adicionar outra musica?(s/n)";
+        cin >> continuar;
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
     }
 
-    playlist.exibirPlaylist();
+
     system("pause");
     return 0;
 }
